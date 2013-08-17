5c6cddff0f2dd0c8fefae63b634415e38b27e389
Al Chou <achou@unow.com>
2013-08-17 13:00:29 -0700

    Work in progress toward filesystem listener based optimization.

--- IRC_log_reader.rb
+++ IRC_log_reader.rb
@@ -11,17 +11,17 @@ class IRCLogReader < Sinatra::Base
 
   Dir.chdir( LOGFILE_PATH )
   @@log_filenames = Dir.glob( 'IRC.log*' ).sort { |a, b| b <=> a }
-
+cattr_accessor :log_filenames, :log_file_contents
   @@log_file_contents = {}
 
 
   template :index do
-      #@@log_filenames[1..-1].unshift( filenames[0] ).each do |filename|
     t = <<-EOT
     <%
       Dir.chdir( LOGFILE_PATH )
       filenames = Dir.glob( 'IRC.log*' )
-      filenames[1..-1].sort { |a, b| b <=> a }.unshift( filenames[0] ).each do |filename|
+      IRCLogReader.log_filenames[1..-1].unshift( IRCLogReader.log_filenames[0] ).each do |filename|
+      #filenames[1..-1].sort { |a, b| b <=> a }.unshift( filenames[0] ).each do |filename|
     %>
       <a href='<%= url(filename) %>'><%= filename %></a><br />
     <% end %>
@@ -39,11 +39,11 @@ class IRCLogReader < Sinatra::Base
   get '/:filename' do
     filename = params[:filename]
 
-    unless @file_contents = @@log_file_contents[filename]
+    unless @file_contents = IRCLogReader.log_file_contents[filename]
       @file_contents = File.open( File.expand_path( filename, LOGFILE_PATH ) ) { |file| file.read }
 
       if filename =~ /\.\d+\Z/
-        @@log_file_contents[filename] = @file_contents
+        IRCLogReader.log_file_contents[filename] = @file_contents
       end
     end
 
@@ -53,8 +53,9 @@ end
 
 
 Listen.to( LOGFILE_PATH, :filter => /\AIRC\.log\./, :relative_paths => TRUE ) do |modified_paths, added_paths, removed_paths|
-  @@log_filenames += ( added_paths + removed_paths ) #.map { |path| File.basename( path ) }
-  @@log_filenames.sort! { |a, b| b <=> a }
+  IRCLogReader.log_filenames -= removed_paths
+  IRCLogReader.log_filenames += added_paths
+  IRCLogReader.log_filenames.sort! { |a, b| b <=> a }
 end
 
 
